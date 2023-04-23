86f720bb10fd75ccaffc7ea849710ebf6d7b0817
Yunir <spire47911@yandex.ru>
2023-04-23 22:07:35 +0500

    Task24v2

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,57 +1,74 @@
 import java.util.*;
 public class PhoneBook {
     public static Map<String, String> mapPhoneBook = new TreeMap<>();
+    private static final String NAME_REGEX = "[А-Яа-я]+";
+    private static final String NUMBER_REGEX = "[\\d]{11}";
     public static void main(String[] args) {
-        System.out.println("Введите номер,имя или команду:");
-        while (true){
-            Scanner scanner = new Scanner(System.in);
-            String text = scanner.nextLine();
 
+        while (true){
+            System.out.println("Введите номер,имя или команду:");
+            String text = new Scanner(System.in).nextLine();
             if (text.toLowerCase().startsWith("list")) {
-                System.out.println(getAllContacts());
+               printAllContacts();
             }
-            addContactByPhone(text);
-            addContactByName(text);
+
+            else if (text.toLowerCase().matches(NUMBER_REGEX)){
+                addContactByPhone(text);
+            }
+
+            else if (text.toLowerCase().matches(NAME_REGEX)){
+                addContactByName(text);
+            }else{
+                System.out.println("Неверный формат ввода");
+                  return;
+            }
         }
     }
     public static void addContactByPhone(String phone) {
-        Scanner scanner = new Scanner(System.in);
-        if (phone.matches("[\\d]{11}")) {
+
             if (mapPhoneBook.containsKey(phone)) {
                 System.out.println(phone+" "+mapPhoneBook.get(phone));
-            } else {
-                System.out.println("Такого номера нет в телефонной книге.Введите имя абонента для номера "+phone);
-                String name = scanner.nextLine();
-                mapPhoneBook.put(phone, name);
-                System.out.println("Контакт сохранен");
+                return;
             }
-        }
-    }
-    public static Set<String> getAllContacts() {
+                System.out.println("Такого номера нет в телефонной книге.Введите имя абонента для номера "+phone);
+                String name = new Scanner(System.in).nextLine();
+                if(name.matches(NAME_REGEX)){
+                    mapPhoneBook.put(phone, name);
+                    System.out.println("Контакт сохранен");
+                }else {
+                    System.out.println("Неправильно введено имя");
+                }
 
-        Set<String> setPhoneBook = new TreeSet<>();
-
-        for (String key : mapPhoneBook.keySet()) {
-            setPhoneBook.add(mapPhoneBook.get(key) + " - " + key);
-        }
-        return setPhoneBook;
     }
+
     public static void addContactByName(String name) {
-        Scanner scanner = new Scanner(System.in);
-        if (name.matches("[А-Яа-я]+")) {
+
             if (mapPhoneBook.containsValue(name)) {
+                System.out.print(name + ": ");
                 for (String tel : mapPhoneBook.keySet()) {
                     if (mapPhoneBook.get(tel).equals(name)) {
-                        System.out.println(tel+" "+name);
+                        System.out.print(tel + " ");
                     }
                 }
-            } else {
+                return;
+            }
                 System.out.println("Такого имени в телефонной книге нет.");
                 System.out.println("Введите номер телефона для абонента "+name+":");
-                String phone = scanner.nextLine();
-                mapPhoneBook.put(phone, name);
-                System.out.println("Контакт сохранен");
-            }
+                String phone = new Scanner(System.in).nextLine();
+                if(phone.matches(NUMBER_REGEX)){
+                    mapPhoneBook.put(phone, name);
+                    System.out.println("Контакт сохранен");
+                }else{
+                    System.out.println("Неправильно введен номер");
+                }
+
+    }
+    public static void printAllContacts() {
+
+        if(mapPhoneBook.isEmpty()){
+            System.out.println("Книга контактов пуста");
+            return;
         }
+        mapPhoneBook.forEach((key,value)-> System.out.println(value+" - "+key));
     }
 }
