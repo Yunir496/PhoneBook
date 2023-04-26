c2d1d5382e4f293929dc7f3fa414410fc53afe22
Yunir <spire47911@yandex.ru>
2023-04-26 22:53:10 +0500

    Task24v6

--- src/PhoneBookSet.java
+++ src/PhoneBookSet.java
@@ -15,6 +15,10 @@ public class PhoneBookSet {
                 addContactByPhone(text);
             } else if (text.toLowerCase().matches(NAME_REGEX)) {
                 addContactByName(text);
+            } else if(text.toLowerCase().matches("удалить "+NUMBER_REGEX)){
+                removeContactByPhone(text.split(" ",2)[1]);
+            } else if(text.toLowerCase().matches("удалить "+NAME_REGEX)){
+                removeContactByName(text.split(" ",2)[1]);
             } else {
                 System.out.println("Неверный формат ввода");
                 return;
@@ -87,103 +91,29 @@ public class PhoneBookSet {
         }
         mapPhoneBook.forEach((key, value) -> System.out.println(key + " - " + value));
     }
-}
-
-
-/*import java.util.*;
-public class PhoneBookSet {
-    public static Map<String, TreeSet<String>> mapPhoneBook = new TreeMap<>();
-    private static final String NAME_REGEX = "[А-Яа-я]+";
-    private static final String NUMBER_REGEX = "[\\d]{11}";
-
-    public static void main(String[] args) {
-        while (true) {
-            System.out.println("Введите номер,имя или команду:");
-            String text = new Scanner(System.in).nextLine();
-            if (text.toLowerCase().startsWith("list")) {
-                printAllContacts();
-            }
-            else if (text.toLowerCase().matches(NUMBER_REGEX)){
-                addContactByPhone(text);
-               }
-            else if (text.toLowerCase().matches(NAME_REGEX)) {
-                addContactByName(text);
-            } else {
-                System.out.println("Неверный формат ввода");
-                return;
-            }
+    public static void removeContactByName(String name){
+        if (!mapPhoneBook.containsKey(name)) {
+            System.out.println("Такого контакта нет");
+            return;
         }
+        mapPhoneBook.remove(name);
+        System.out.println("Контакт успешно удален");
     }
-
-
-    public static void addContactByPhone(String phone) {
+    public static void removeContactByPhone(String phone){
+        boolean flag = false;
         for (Map.Entry<String, TreeSet<String>> entry : mapPhoneBook.entrySet()) {
             if (entry.getValue() != null && entry.getValue().contains(phone)) {
-                System.out.println("Такой номер уже есть у другого контакта");
-                System.out.println(entry.getKey());
-                System.out.println(phone);
-                return;
+                entry.getValue().remove(phone);
+                System.out.println("Номер из контакта удален");
+                flag = true;
             }
         }
-        System.out.println("Такого номера нет в телефонной книге.Введите имя контакта для номера " + phone);
-        String name = new Scanner(System.in).nextLine();
-        TreeSet<String> phones = new TreeSet();
-        if (mapPhoneBook.containsKey(name)) {
-            System.out.println("Контакт с таким именем уже существует");
-            return;
-        }
-          else if (name.matches(NAME_REGEX)) {
-                mapPhoneBook.put(name, phones);
-                phones.add(phone);
-                System.out.println("Контакт сохранен");
-            } else {
-                System.out.println("Неправильно введено имя");
-            }
-        }
-
-
-    public static void addContactByName(String name) {
-        if (mapPhoneBook.containsKey(name)) {
-            System.out.println("Найден контакт с таким именем");
-            System.out.print(name + ": ");
-            for (String s : mapPhoneBook.get(name)) {
-                System.out.println(s + " ");
-            }
-            return;
-        } else {
-            System.out.println("Такого имени в телефонной книге нет.\n" + "Введите номер телефона для контакта " + name + ":");
-            String phone = new Scanner(System.in).nextLine();
-            for (Map.Entry<String, TreeSet<String>> entry : mapPhoneBook.entrySet()) {
-                if (entry.getValue() != null && entry.getValue().contains(phone)) {
-                    System.out.println("Такой номер уже есть у другого контакта");
-                    System.out.println(entry.getKey());
-                    System.out.println(phone);
-                    return;
-                }
-            }
-        TreeSet<String> phones = new TreeSet();
-        if (phone.matches(NUMBER_REGEX)) {
-            mapPhoneBook.put(name, phones);
-            phones.add(phone);
-            System.out.println("Контакт сохранен");
-        } else {
-            System.out.println("Неправильно введен номер");
+        if(!flag){
+            System.out.println("Такого номера нет в списке контактов");
         }
     }
-}
-
-
-    public static void printAllContacts() {
-        if(mapPhoneBook.isEmpty()){
-            System.out.println("Книга контактов пуста");
-            return;
-        }
-        mapPhoneBook.forEach((key,value)-> System.out.println(key+" - "+value));
     }
 
 
-    public static void editContact(Map<String, TreeSet<String>> contacts){
 
-    }
-}*/
 
